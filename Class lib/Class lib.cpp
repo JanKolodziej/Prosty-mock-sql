de0ce911388ce9b95f17c3e3b687bbbd923d0d49
JanKolodziej <jankolodziej@outlook.com>
2025-12-16 17:45:13 +0100

    Dodaj pliki projektów.

---
+++ Class lib/Class lib.cpp
@@ -0,0 +1,10 @@
+﻿// Class lib.cpp : Definiuje funkcje biblioteki statycznej.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: To jest przykład funkcji biblioteki
+void fnClasslib()
+{
+}
